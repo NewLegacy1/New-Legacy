522919939594460670e36e96edae98ba238c2294
Your Name <you@example.com>
2026-02-02 12:34:38 -0500

    Initial commit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qgVp1Fioa8iOajhJ4C8+X2Xkfqg27AHjSjHB2rbLx98=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"76hRg8AmwRl4sjhniKI+7qckvK0vYjn4MNDxezb/c2o=\"\n}"
